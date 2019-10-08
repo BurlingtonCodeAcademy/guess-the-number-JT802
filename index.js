e411db3f3154292ffe79e101d00bd10c696387b2
JRT <jonathan.tresch@gmail.com>
2019-10-08 19:18:16 -0400

    Made additional changes so story runs

--- index.js
+++ index.js
@@ -12,19 +12,23 @@ start();
 async function start() {
 
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.");
-  const min = 1;
-  const max = 100;
-  const guess = (Math.floor(Math.random() * (max - min + 1)) + min);
-  humanResponse = ("");
+  let min = 1;
+  let max = 100;
+  let guess = (Math.floor(Math.random() * (max - min + 1)) + min);
   
   let compQuestion = await ask("Is your secret number: " + guess + "?" + "\n(Y)es or (N)o \n"); {
   
   console.log(compQuestion);
-    while (compQuestion = await ask("Is your secret number: " + guess + "?" + "\n(Y)es or (N)o \n")) { 
-        if (humanResponse = await ask("Is it ... " + guess + "?")) {
-            return console.log(compQuestion);
-         } else {
-            return console.log("I did it! I did it! Computers rule and humans drool.");   
+    while (compQuestion === "N") {
+      let higherLower = await ask("Is the number (H)igher or (L)ower: "); 
+        if (higherLower === "H") {
+          min = guess;
+          guess = (Math.floor(Math.random() * (max - min + 1)) + min)
+          compQuestion = await ask("Is your secret number: " + guess + "?" + "\n(Y)es or (N)o \n");
+         } else if (higherLower === "L") {
+          max = guess;
+          guess = (Math.floor(Math.random() * (max - min + 1)) + min);
+          compQuestion = await ask("Is your secret number: " + guess + "?" + "\n(Y)es or (N)o \n");;   
             }
           } process.exit();
         }
