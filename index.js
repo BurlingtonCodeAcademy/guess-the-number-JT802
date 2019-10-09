2d3a4fd8fedf36403fc7ba0ddd0bfead3f3abcdc
Habenzy <robertrstauss@gmail.com>
2019-10-09 11:36:15 -0400

    branch for feedback pr

--- index.js
+++ index.js
@@ -10,26 +10,9 @@ function ask(questionText) {
 start();
 
 async function start() {
-
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.");
-  let min = 1;
-  let max = 100;
-  let guess = (Math.floor(Math.random() * (max - min + 1)) + min);
-  
-  let compQuestion = await ask("Is your secret number: " + guess + "?" + "\n(Y)es or (N)o \n"); {
-  
-  console.log(compQuestion);
-    while (compQuestion === "N") {
-      let higherLower = await ask("Is the number (H)igher or (L)ower: "); 
-        if (higherLower === "H") {
-          min = guess;
-          guess = (Math.floor(Math.random() * (max - min + 1)) + min)
-          compQuestion = await ask("Is your secret number: " + guess + "?" + "\n(Y)es or (N)o \n");
-         } else if (higherLower === "L") {
-          max = guess;
-          guess = (Math.floor(Math.random() * (max - min + 1)) + min);
-          compQuestion = await ask("Is your secret number: " + guess + "?" + "\n(Y)es or (N)o \n");;   
-            }
-          } process.exit();
-        }
-      }
+  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
+  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
+  console.log('You entered: ' + secretNumber);
+  // Now try and complete the program.
+  process.exit();
+}
